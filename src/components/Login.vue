b3d45871fcefcdfd2f08c96090c913a1b9df540a
satori <2037177696@qq.com>
2020-06-16 10:16:41 +0800

    登录功能实现

---
+++ src/components/Login.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form
+        ref="loginFromRef"
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            placeholder="请输入用户名"
+            prefix-icon="iconfont icon-user"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            placeholder="请输入密码"
+            prefix-icon="iconfont icon-3702mima"
+            v-model="loginForm.password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginFrom">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登陆表单的数据绑定对象
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          //   { require: true, message: '请输入用户名', trigger: 'blur' },
+          //   { min: 3, max: 10, message: '长度在3-10之间', trigger: 'blur'}
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登陆表单
+    resetLoginFrom() {
+      this.$refs.loginFromRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFromRef.validate(async vaild => {
+        if (!vaild) return false
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    padding: 10px;
+    border-radius: 50%;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    background-color: #fff;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  width: 100%;
+  padding: 0 10px;
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
